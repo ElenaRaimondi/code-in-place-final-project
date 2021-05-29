6f2696892338d5ef9be208a1a23da1245b8392df
Elena Raimondi <eln.raimondi@gmail.com>
2021-05-29 15:17:43 +0100

    Add vocab function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 DECK_FILE_PATH = "decks/italian_to_english.json"
 NUMBER_OF_ROUNDS = 3
+NUMBER_OF_ATTEMPTS = 2
 
 
 def intro():
@@ -14,28 +15,37 @@ def intro():
 
 
 def load_deck():
-  with open(DECK_FILE_PATH) as deck_file:
-    deck = json.load(deck_file)
-    return deck
+    with open(DECK_FILE_PATH) as deck_file:
+        deck = json.load(deck_file)
+        return deck
 
 
 def practice_card_vocab(card):
-  pass
+    for attempt in range(NUMBER_OF_ATTEMPTS):
+        guess = input(
+            f"How do`` you say {card['native']} in your target language? ")
+        if guess == card["target"]:
+            print("Bravo!")
+            return
+        else:
+            print("Try again!")
+
+    print(f"{card['native']} is {card['target']}. Better luck next time!")
 
 
 def practice_card_tenses(card):
-  pass
+    pass
 
 
 def practice_deck(deck):
     score = 0
 
-    for round in range (NUMBER_OF_ROUNDS):
-      card = random.choice(deck["cards"])
-      if card["type"] == "vocab":
-        practice_card_vocab(card)
-      elif card["type"] == "tenses":
-        practice_card_tenses(card)
+    for round in range(NUMBER_OF_ROUNDS):
+        card = random.choice(deck["cards"])
+        if card["type"] == "vocab":
+            practice_card_vocab(card)
+        elif card["type"] == "tenses":
+            practice_card_tenses(card)
 
 
 def main():
