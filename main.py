54e75b354d90d8b16b7f429b67c6ba2ff19c2747
Elena Raimondi <eln.raimondi@gmail.com>
2021-05-29 12:52:07 +0100

    Extract play new game

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 CARDS_FILE_PATH = "cards/italian_to_english.json"
 
-def main():
-  user_name, score = intro()
-  input(f"Ready, {user_name}? Print enter to start! ")
-
-
-
-
 def intro():
   print("Benvenuto!")
   user_name = input("Come ti chiami? ")
-  score = 0
   print("Rules")
-  return [user_name, score]
+  return user_name
+
+def play_new_game():
+  score = 0
+
+
+def main():
+  user_name = intro()
+  input(f"Ready, {user_name}? Print enter to start! ")
+  play_new_game()
+
 
 
 if __name__ == "__main__":
